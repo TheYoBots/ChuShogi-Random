ca2b7afcad0f1292a34db374fdd84e1c56945d6f
Yohaan Seth Nathan <73843275+TheYoBots@users.noreply.github.com>
2023-05-12 12:17:17 +0000

    Parse sfen moves

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # ChuShogi Random
-This is my attempt to create a simple USI chushogi engine meant to play only Random moves.
+This is my attempt to create a simple USI chushogi engine meant to play only Random moves. The provided code does not have explicit knowledge of where each piece is or how each piece moves. It only generates random moves without considering the current position or piece movements.
+
+## USI Protocol Implementation
+An example of the usi commands execution is shown below. The bestmove is provided in the usi format. To execute usi commands you can eirther compile and run the [`chushogi.py`](chushogi.py) file or run `python3 chushogi.py` in your terminal.
+```bash
+< usi
+
+> id name ChuShogi Random
+> id author Yohaan Seth Nathan
+> usiok
+
+< isready
+
+> readyok
+
+< usinewgame
+< position lfcsgekgscfl/a1b1txot1b1a/mvrhdqndhrvm/pppppppppppp/3i4i3/12/12/3I4I3/PPPPPPPPPPPP/MVRHDNQDHRVM/A1B1TOXT1B1A/LFCSGKEGSCFL b - 1
+< go
+
+> bestmove k7l6
+```
--- chushogi.py
+++ chushogi.py
@@ -45,8 +45,25 @@ def make_move(board, move):
     board[dest_row][dest_col] = board[src_row][src_col]
     board[src_row][src_col] = '.'
 
+# Function to parse SFEN and set up the board
+def parse_sfen(sfen):
+    board = [['.' for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
+    sfen_rows = sfen.split('/')
+    row_index = 0
+    for sfen_row in sfen_rows:
+        col_index = 0
+        for char in sfen_row:
+            if char.isdigit():
+                col_index += int(char)
+            else:
+                board[row_index][col_index] = char
+                col_index += 1
+        row_index += 1
+    return board
+
 # Function to parse a USI command
 def parse_usi_command(command):
+    global board  # Make the 'board' variable accessible within the function
     tokens = command.split()
     if tokens[0] == 'usi':
         print("id name ChuShogi Random")
@@ -55,28 +72,37 @@ def parse_usi_command(command):
     elif tokens[0] == 'isready':
         print("readyok")
     elif tokens[0] == 'usinewgame':
-        pass
+        board = initialize_board()  # Initialize the board
     elif tokens[0] == 'position':
         if tokens[1] == 'startpos':
-            # Set up the initial position
-            board = initialize_board()
+            board = initialize_board()  # Initialize the board
         else:
+            # Parse the starting position
+            sfen = ' '.join(tokens[1:])
+            sfen_parts = sfen.split(' ')
+            board = parse_sfen(sfen_parts[0])
             # Handle additional position setup commands if required
             pass
     elif tokens[0] == 'go':
         # Generate and make a move
         move = get_random_move(board)
         make_move(board, move)
-        print(f"bestmove {move[1]}{move[0]}{move[3]}{move[2]}")
+        src_col, src_row, dest_col, dest_row = move
+        usi_move = f"bestmove {chr(src_col + 97)}{src_row + 1}{chr(dest_col + 97)}{dest_row + 1}"
+        print(usi_move)
     elif tokens[0] == 'quit':
         quit()
 
 # Main game loop
 def main():
-    board = initialize_board()
-    while True:
-        command = input()
-        parse_usi_command(command)
+    global board  # Make the 'board' variable accessible within the function
+    board = initialize_board()  # Initialize the board
+    try:
+        while True:
+            command = input()
+            parse_usi_command(command)
+    except KeyboardInterrupt:
+        quit()
 
 if __name__ == '__main__':
     main()
